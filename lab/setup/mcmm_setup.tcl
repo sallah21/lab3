700d218dcefdb94fc628eb3a5b79b7f305291634
sallach21 <luckyluj24@gmail.com>
2025-05-28 10:36:55 +0200

    initial

---
+++ lab/setup/mcmm_setup.tcl
@@ -0,0 +1,20 @@
+#####################################################################
+#####						MCMM Setup Script					#####
+#####################################################################
+
+# Remove all MCMM related info
+
+
+# Create Corner(s)
+
+
+# Set parasitics parameters
+
+# Create Mode
+
+
+# Create Scenario(s)
+
+
+# Set operating conditions for the corner and scenario
+
---
+++ lab/setup/technology_setup.tcl
@@ -0,0 +1,19 @@
+#####################################################################
+#####			Technology Setup Script			                #####
+#####################################################################
+
+set_attribute [get_site_defs unit] symmetry Y
+set_attribute [get_site_defs unit] is_default true
+
+if {[string equal tlup ${ParasiticModel}]} {
+	read_parasitic_tech -layermap ${LayerMapFile} -tlup ${TlupMaxFile} -name maxTLU
+	read_parasitic_tech -layermap ${LayerMapFile} -tlup ${TlupNomFile} -name nomTLU
+	read_parasitic_tech -layermap ${LayerMapFile} -tlup ${TlupMinFile} -name minTLU
+} elseif {[string equal nxtgrd ${ParasiticModel}]} {
+	read_parasitic_tech -layermap ${LayerMapFile} -tlup ${NxtGrdMaxFile} -name maxTLU
+	read_parasitic_tech -layermap ${LayerMapFile} -tlup ${NxtGrdNomFile} -name nomTLU
+	read_parasitic_tech -layermap ${LayerMapFile} -tlup ${NxtGrdMinFile} -name minTLU
+} 
+
+set_attribute [get_layers {M1 M3 M5 M7 M9}]   routing_direction vertical
+set_attribute [get_layers {M2 M4 M6 M8 MRDL}] routing_direction horizontal
